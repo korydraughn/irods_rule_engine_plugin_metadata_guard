650f5d6920fe4b171dc9ba7b36cfe37d23b01f1b
Kory Draughn <korydraughn@renci.org>
2020-02-11 17:24:02 -0500

    Adjusted build script to include revision number.
    Also, corrected file name reference.

--- packaging/run_metadata_guard_test.py
+++ packaging/run_metadata_guard_test.py
@@ -1,5 +1,5 @@
 import subprocess
 
 if __name__ == "__main__":
-    subprocess.call(['sudo', 'python', '-m', 'xmlrunner', 'irods.test.test_rule_engine_plugin_update_collection_mtime'])
+    subprocess.call(['sudo', 'python', '-m', 'xmlrunner', 'irods.test.test_rule_engine_plugin_metadata_guard'])
 
